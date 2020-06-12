6166e3f135d495d50893b3363d99f9223abc45cd
Cesar Plasencia <cplasenciajr@gmail.com>
2020-06-11 19:02:44 -0700

    First draft of week2.py script

--- week2/week2.py
+++ week2/week2.py
@@ -5,30 +5,21 @@ import requests
 import re
 import logging
 import sys
+import json
 
 logging.basicConfig(level=logging.DEBUG, format='%(levelname)s %(asctime)s %(message)s')
 
-def get_directory(argv):
-    logging.debug('Determining if argv is empty of is not a valid directory')
-    if not argv:
-        logging.debug('argv is empty. Defaulting to current working directory')
-        return os.getcwd()
-    elif not os.path.isdir(argv):
-        logging.error('argv is not a valid directory. Defauling to current working directory: {}'.format(os.getcwd()))
-        return os.getcwd()
-    logging.debug('Directory {} is valid'.format(argv))
-    return argv
-
 def get_directory_of_files(directory):
     logging.debug('Building list of files in directory {}'.format(directory))
-    list_of_files = [f for f in os.listdir(directory) if os.path.isfile(f)]
+    list_of_files = [f for f in os.listdir(directory) if os.path.isfile('{}/{}'.format(directory, f))]
+    logging.debug('List of files in directory: {}'.format(list_of_files))
     if not list_of_files:
         logging.critical('There are no files in the directory {}'.format(directory))
         raise ValueError('There are no files in the directory')
     logging.debug('List of files found: {}'.format(list_of_files))
     return list_of_files
 
-def process_into_dictionary(list_of_f):
+def process_into_dictionary(list_of_f, directory):
     logging.debug('Generating empty dictionary to append responses to')
     response_dict = []
     entry_format = ('title', 'name', 'date', 'feedback')
@@ -36,7 +27,7 @@ def process_into_dictionary(list_of_f):
     logging.debug('Iterating over: {}'.format(list_of_f))
     for f in list_of_f:
         logging.debug('Opening {} as a file'.format(f))
-        with open(f, 'r') as entry:
+        with open('{}/{}'.format(directory, f), 'r') as entry:
             logging.debug('Creating empty dictionary template')
             dict_template = {}
             content = [line.strip() for line in entry.readlines()]
@@ -52,23 +43,23 @@ def process_into_dictionary(list_of_f):
     logging.debug('response_dict built: {}'.format(response_dict))
     return response_dict
 
-def upload_to_web(response_dict, url):
-    logging.debug('Generating POST to URL {}'.format(url))
-    response = requests.post(url, data=response_dict)
-    if not response.ok:
-        raise Exception('POST failed with status code {}'.format(response.status_code))
-    logging.debug('POST succeeded with status code {}'.format(response.status_code))
+def upload_to_web(response_dict):
+    url = 'http://34.72.87.22/feedback/'
+    for entry in response_dict:
+        response = requests.post(url, json=entry)
+        if not response.ok:
+            raise Exception('POST failed with status code {}'.format(response.status_code))
+        logging.debug('POST succeeded with status code {}'.format(response.status_code))
     return 0
 
-def main(argv):
+def main():
     logging.debug('Begin script')
-    url = input('Enter URL to post to: ')
-    directory = get_directory(argv)
+    directory = '/data/feedback'
     list_of_files = get_directory_of_files(directory)
-    response_dict = process_into_dictionary(list_of_files)
-    exit_code = upload_to_web(response_dict, url)
+    response_dict = process_into_dictionary(list_of_files, directory)
+    exit_code = upload_to_web(response_dict)
     logging.debug('Ending script with exit code {}'.format(exit_code))
     sys.exit(exit_code)
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main()
